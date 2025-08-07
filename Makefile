b76b069327f6eb1256ef35a3372885d37d95799c
Tiago Angelo <kurtis.angelo@gmail.com>
2025-08-07 11:21:22 -0300

    Twelfth commit

--- Makefile
+++ Makefile
@@ -15,6 +15,12 @@ COMPOSE_FILE = docker-compose.yml
 help: ## Display this help.
 	@awk 'BEGIN {FS = ":.*##"; printf "\nUsage:\n  make \033[36m<target>\033[0m\n"} /^[a-zA-Z_0-9-]+:.*?##/ { printf "  \033[36m%-15s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
 
+##@ Wire
+
+.PHONY: wire
+wire: ## Generate wire dependency injection code
+	go tool -modfile=tools.mod wire ./cmd/app
+
 ##@ Docker Compose
 
 .PHONY: up
--- cmd/app/wire_provs.go
+++ cmd/app/wire_provs.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/google/wire"
 
+	"github.com/angelokurtis/rest-api-concurrency/internal/httpserver"
 	"github.com/angelokurtis/rest-api-concurrency/internal/term"
 	"github.com/angelokurtis/rest-api-concurrency/pkg/app"
 )
@@ -10,6 +11,7 @@ import (
 //nolint:unused // This function is used during compile-time to generate code for dependency injection
 var providers = wire.NewSet(
 	term.Providers,
+	httpserver.Providers,
 	wire.Bind(new(Runner), new(*app.Runner)),
 	wire.Bind(new(term.Renderer), new(*term.MarkdownRenderer)),
 	wire.Struct(new(app.Runner), "*"),
