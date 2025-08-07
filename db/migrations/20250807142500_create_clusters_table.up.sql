617c45b74a24618f042c7c1236b9717721420631
Tiago Angelo <kurtis.angelo@gmail.com>
2025-08-07 15:26:11 -0300

    Thirty-first commit

--- db/migrations/20250807142500_create_clusters_table.up.sql
+++ db/migrations/20250807142500_create_clusters_table.up.sql
@@ -3,10 +3,11 @@ EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE clusters
 (
-    id             UUID PRIMARY KEY   DEFAULT gen_random_uuid(),
-    name           TEXT      NOT NULL,
-    api_server_url TEXT      NOT NULL,
-    token          TEXT      NOT NULL,
-    created_at     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    id         UUID PRIMARY KEY   DEFAULT gen_random_uuid(),
+    name       TEXT      NOT NULL,
+    version    TEXT      NOT NULL,
+    provider   TEXT      NOT NULL,
+    region     TEXT      NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
