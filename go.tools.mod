0e67d03271939e884d779be945e0e26244ee2093
Tiago Angelo <kurtis.angelo@gmail.com>
2025-08-07 11:10:02 -0300

    Tenth commit

--- go.tools.mod
+++
@@ -1,3 +0,0 @@
-module github.com/angelokurtis/rest-api-concurrency/tools
-
-go 1.24.6
--- scripts/init_tools_mod.sh
+++ scripts/init_tools_mod.sh
@@ -3,13 +3,13 @@
 set -euo pipefail
 
 # Initialize a new Go module for tools
-rm -rf go.tools.mod go.tools.sum
+rm -rf tools.mod tools.sum
 TMP_DIR=$(mktemp -d)
 cd "$TMP_DIR"
 go mod init github.com/angelokurtis/rest-api-concurrency/tools
-mv go.mod "$OLDPWD/go.tools.mod"
+mv go.mod "$OLDPWD/tools.mod"
 cd "$OLDPWD"
 rm -rf "$TMP_DIR"
 
 # Install tools
-go get -modfile=go.tools.mod -tool github.com/golang-migrate/migrate/v4/cmd/migrate@latest
+go get -modfile=tools.mod -tool github.com/golang-migrate/migrate/v4/cmd/migrate@latest
